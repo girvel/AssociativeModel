c1990d21f1c6da66dcd27f269c7eb12103824dc0
widauka@ya.ru <bIg-REED-bluE_flowers73>
2020-07-26 23:38:55 +0500

    Initial

---
+++ AssociativeModel/Net.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AssociativeModel
+{
+    public class Net<T>
+    {
+        private readonly Dictionary<T, List<T>> _dependencies = new Dictionary<T, List<T>>();
+        
+        public T Root { get; }
+
+        public Net(T root)
+        {
+            Root = root;
+            _dependencies[root] = new List<T>();
+        }
+
+        public void AddDependency(T first, T second)
+        {
+            var (parent, child) = _dependencies.ContainsKey(first) ? (first, second) : (second, first);
+            
+            if (!_dependencies.ContainsKey(parent))
+            {
+                throw new ArgumentException("Net must contain parent node.");
+            }
+
+            if (!_dependencies.ContainsKey(child))
+            {
+                _dependencies[child] = new List<T>();
+            }
+
+            if (!_dependencies[parent].Contains(child))
+            {
+                _dependencies[parent].Add(child);
+            }
+
+            if (!_dependencies[child].Contains(parent))
+            {
+                _dependencies[child].Add(parent);
+            }
+        }
+        
+        public T[] GetDependencies(T node)
+        {
+            return _dependencies[node].ToArray();
+        }
+
+        public void RemoveDependency(T first, T second)
+        {
+            if (!_dependencies.ContainsKey(first) || !_dependencies.ContainsKey(second))
+            {
+                throw new ArgumentException("Net must contain both nodes");
+            }
+
+            _dependencies[first].Remove(second);
+            _dependencies[second].Remove(first);
+
+            if (!_dependencies[first].Any() && !first.Equals(Root)) _dependencies.Remove(first);
+            if (!_dependencies[second].Any() && !second.Equals(Root)) _dependencies.Remove(second);
+        }
+    }
+}
---
+++ AssociativeModel/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AssociativeModel
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var net = new Net<string>("root");
+            var currentNode = net.Root;
+            var selectedIndex = 0;
+            var active = true;
+            var history = new Stack<string>();
+            
+            while (active)
+            {
+                Console.Clear();
+                Console.WriteLine($"--- {currentNode} ---\n");
+                
+                var i = 0;
+                var dependencies = net.GetDependencies(currentNode);
+                foreach (var node in dependencies)
+                {
+                    Console.ForegroundColor = i == selectedIndex ? ConsoleColor.White : ConsoleColor.Gray;
+                    Console.WriteLine(node);
+                    i++;
+                }
+                
+                Console.ResetColor();
+
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (selectedIndex > 0) selectedIndex--;
+                        break;
+                    
+                    case ConsoleKey.DownArrow:
+                        if (selectedIndex < dependencies.Length - 1) selectedIndex++;
+                        break;
+                    
+                    case ConsoleKey.Enter:
+                        if (!dependencies.Any()) break;
+                        currentNode = dependencies[selectedIndex];
+                        selectedIndex = 0;
+                        break;
+                    
+                    case ConsoleKey.A:
+                        Console.Write("Name: ");
+                        net.AddDependency(currentNode, Console.ReadLine());
+                        break;
+                    
+                    case ConsoleKey.Delete:
+                        if (!dependencies.Any()) break;
+                        
+                        Console.Write($"Remove node {dependencies[selectedIndex]}? [Y/n]");
+                        
+                        if (Console.ReadKey().Key != ConsoleKey.N) 
+                            net.RemoveDependency(currentNode, dependencies[selectedIndex]);
+                        break;
+                }
+            }
+        }
+    }
+}
